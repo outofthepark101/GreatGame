deac8602ceed256b0c6015cd9baf8c944a104121
Byungsung Park <outofthepark101@gmail.com>
2024-04-20 00:30:20 +0900

    Setting up dialogue system and ink

---
+++ GreatGame/Assets/Plugins/Pixel Crushers/Dialogue System/Third Party Support/Ink Support/Scripts/BarkInkOnIdle.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using Ink.Runtime;
+
+namespace PixelCrushers.DialogueSystem.InkSupport
+{
+
+    public class BarkInkOnIdle : BarkOnIdle
+    {
+
+        [Tooltip("Bark this knot. Can also specify 'knot.stitch' to jump to a stitch in the knot. If blank, bark from the beginning.")]
+        public string barkKnot;
+
+        private int progress = 0;
+
+        public override void TryBark(Transform target, Transform interactor)
+        {
+            Story story = DialogueSystemInkIntegration.LookupStory(conversation);
+            if (story == null) return;
+            story.ResetState();
+            if (!string.IsNullOrEmpty(barkKnot))
+            {
+                story.ChoosePathString(barkKnot);
+            }
+            if (!story.canContinue) return;
+
+            var barkText = story.Continue().Trim();
+            if (barkOrder == BarkOrder.Sequential)
+            {
+                for (int i = 0; i < progress; i++)
+                {
+                    if (!story.canContinue)
+                    {
+                        break;
+                    }
+                    barkText = story.Continue().Trim();
+                }
+                if (story.canContinue)
+                {
+                    progress++;
+                }
+                else
+                {
+                    progress = 0;
+                }
+            }
+
+            DialogueManager.BarkString(barkText, GetBarker());
+        }
+
+    }
+}
---
+++ GreatGame/Assets/Plugins/Pixel Crushers/Dialogue System/Third Party Support/Ink Support/Scripts/Editor/BarkInkOnIdleEditor.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+namespace PixelCrushers.DialogueSystem.InkSupport
+{
+
+    [CustomEditor(typeof(BarkInkOnIdle), true)]
+    public class BarkInkOnIdleEditor : BarkOnIdleEditor
+    {
+        private List<InkEntrypoint> entrypoints;
+        private string[] entrypointStrings;
+        private string[] entrypointFullPaths;
+
+        public void OnEnable()
+        {
+            List<string> fullPaths;
+            entrypoints = InkEditorUtility.GetAllEntrypoints(out fullPaths);
+            entrypointStrings = InkEditorUtility.EntrypointsToStrings(entrypoints);
+            entrypointFullPaths = fullPaths.ToArray();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            serializedObject.Update();
+            DrawInkSpecific();
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        protected void DrawInkSpecific()
+        {
+            EditorGUILayout.LabelField("Ink-Specific", EditorStyles.boldLabel);
+            var barkConversationProperty = serializedObject.FindProperty("conversation");
+            var barkKnotProperty = serializedObject.FindProperty("barkKnot");
+            EditorGUILayout.PropertyField(barkKnotProperty, true);
+            var index = InkEditorUtility.GetEntrypointIndex(barkConversationProperty.stringValue, barkKnotProperty.stringValue, entrypoints);
+            EditorGUI.BeginChangeCheck();
+#if INK_FULLPATHS
+            index = EditorGUILayout.Popup("Entrypoint Picker", index, entrypointFullPaths);
+#else
+            index = EditorGUILayout.Popup("Entrypoint Picker", index, entrypointStrings);
+#endif
+            if (EditorGUI.EndChangeCheck())
+            {
+                barkKnotProperty.stringValue = SetEntrypoint(barkConversationProperty, barkKnotProperty, index);
+            }
+        }
+
+        protected string SetEntrypoint(SerializedProperty conversationProperty, SerializedProperty startAtKnotProperty, int index)
+        {
+            if (!(0 <= index && index < entrypoints.Count)) return string.Empty;
+            var entrypoint = entrypoints[index];
+            conversationProperty.stringValue = entrypoint.story;
+            if (string.IsNullOrEmpty(entrypoint.knot))
+            {
+                startAtKnotProperty.stringValue = string.Empty;
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(entrypoint.stitch))
+                {
+                    startAtKnotProperty.stringValue = entrypoint.knot;
+                }
+                else
+                {
+                    startAtKnotProperty.stringValue = (entrypoints[index].knot + "." + entrypoints[index].stitch);
+                }
+            }
+            return startAtKnotProperty.stringValue;
+        }
+
+    }
+}
